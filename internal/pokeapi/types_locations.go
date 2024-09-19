f0f89c2c7776885ae8173730b26be383dafd2cc0
drewpayton <drewpayton18@gmail.com>
2024-09-18 22:00:34 -0500

    adding the location struct to be able to pull all the pokemon in a certain area

--- internal/pokeapi/types_locations.go
+++ internal/pokeapi/types_locations.go
@@ -10,3 +10,55 @@ type RespShallowLocations struct {
 		URL  string `json:"url"`
 	} `json:"results"`
 }
+
+type Location struct {
+	EncounterMethodRates []struct{
+		EncounterMethod struct {
+			Name string `json:"name"`
+			URL  string `json:"url"` 
+		} `json:"encounter_method"`
+		VersionDetails struct {
+			Rate    int `json:"rate"`
+			Version struct{
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			}`json:"version"`
+		} `json:"version_deatils"`
+	}`json:"encounter_method_rates"`
+	GameIndex int `json:"game_index"`
+	ID int `json:"id"`
+	Location struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"location"`
+	Names []struct {
+		Name string `json:"name"`
+		Language struct{
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+	}`json:"names"`
+	PokemonEncounter []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"pokemon"`
+		VersionDetails []struct {
+			Version struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			}`json:"version"`
+			MaxChance int `json:"max_chance"`
+			EncounterDetails []struct {
+				MinLevel int `json:"min_level"`
+				MaxLevel int `json:"max_level"`
+				ConditionValues []struct{} `json:"condition_values"`
+				Chance int `json:"chance"`
+				Method struct {
+					Name string `json:"name"`
+					URL  string `json:"url"`
+				} `json:"method"`
+			}`json:"encounter_details"`
+		}`json:"version_details"`
+	} `json:"pokemon_encounters"`
+}
