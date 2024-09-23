c55cf1f4b50a8a47e90599a350195d28c34ed784
Drew Payton <drewpayton18@drews-mbp-2.rush.edu>
2024-09-23 14:16:35 -0500

    added the inspect command to allow user to inspect their caught pokemon

--- command_inspect.go
+++ command_inspect.go
@@ -1,17 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func commandInspect(cfg *config, pokemonName string) error {
 
-	if val, ok := cfg.caughtPokemon[pokemonName]; ok {
-		fmt.Printf("Name: %s\n")
-		fmt.Printf("Height: %s\n")
-		fmt.Printf("Weight: %s\n")
-		fmt.Printf("Stats: %s\n")
-		fmt.Printf("Types: %s\n")
-	} else {
 
+	if val, ok := cfg.caughtPokemon[pokemonName]; ok {
+		fmt.Printf("Name: %s\n", val.Name)
+		fmt.Printf("Height: %d\n", val.Height)
+		fmt.Printf("Weight: %d\n", val.Weight)
+		fmt.Printf("Stats:\n")
+		for _, pokeStats := range val.Stats {
+			fmt.Printf("	-%s: %d\n", pokeStats.Stat.Name, pokeStats.BaseStat)
+		}
+		fmt.Printf("Types:\n")
+		for _, pokeTypes := range val.Types {
+			fmt.Printf("	- %s", pokeTypes.Type.Name)
+		}
+	} else {
+		return errors.New("you have not caught that pokemon")
 	}
-	
+	return nil
 }
