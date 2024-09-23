425b9d97fc6710b6f6bfc66e16d89cfae7650578
drewpayton <drewpayton18@gmail.com>
2024-09-23 00:13:26 -0500

    mostly finished up the Get pokemon api call

--- command_catch.go
+++ command_catch.go
@@ -1,5 +1,10 @@
 package main
 
+import "errors"
+
 func commandCatch(cfg *config, pokemonName string) error{
+	if pokemonName == "" {
+		return errors.New("you need to include a pokemon")
+	}
 	return nil
 }
--- internal/pokeapi/pokemon_get.go
+++ internal/pokeapi/pokemon_get.go
@@ -1,5 +1,36 @@
 package pokeapi
 
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
 func (c *Client) GetPokemon(pokemon string) (Pokemon, error) {
-	return Pokemon{}, nil
+	url := baseURL + "/pokemon/" + pokemon 
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
+	pokemonResp := Pokemon{}
+	err = json.Unmarshal(data, &pokemonResp)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
+	return pokemonResp, nil
 }
